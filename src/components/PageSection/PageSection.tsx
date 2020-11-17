444151a8d236b8d7566f780f607d1e12d45497ca
Mohammad Anees <aman6129@neo.tamu.edu>
2020-11-16 20:50:30 -0600

    Add Order section (#1)
    * add pagesection component, add orders section
    
    * style adjustments

--- src/components/PageSection/PageSection.tsx
+++ src/components/PageSection/PageSection.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const PageSection: React.FC = () => {
-  return null;
+import styles from './PageSection.module.scss';
+
+interface PageSectionProps {
+  height?: string;
+  width?: string;
+  theme?: 'yellow' | 'pink' | 'white';
+  title: string;
+  subtitle?: string;
+  message?: string;
+}
+
+const PageSection: React.FC<PageSectionProps> = ({ 
+  height = '100%',
+  width = 'auto',
+  theme = 'white',
+  title,
+  message,
+  children,
+}) => {
+  return (
+    <div
+      className={[styles.pageContainer, styles[theme]].join(' ')}
+      style={{ height, width }}>
+        <h2 className={styles.sectionTitle}>{title}</h2>
+        {message && <p className={styles.sectionMessage}>{message}</p>}
+        {children}
+    </div>
+  );
 }
 
 export default PageSection;
