76e3716c60ebb08e74bfc4f1354512f705e9f77e
Mohammad Anees <mohammad.irock@gmail.com>
2021-01-01 22:50:17 -0600

    add cakes and pricing info

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as ImageCard } from './ImageCard';
 export { default as Page } from './Page';
 export { default as Footer } from './Footer';
+export { default as EmptySpace } from './EmptySpace';
--- src/pages/cakes.tsx
+++ src/pages/cakes.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
 
-import { Page } from '../components';
+import { Routes } from '../utils/Routes';
+import { Page, ImageCard, EmptySpace } from '../components';
+
+import styles from '../styles/cakes.module.scss';
+
+import CakeImagePlaceholder from '../../static/cakes-page/cakes.png';
 
 const Cakes: React.FC = () => {
-  return <Page />;
+  return (
+    <Page
+      title='The Cakes'
+      subtitle='These are signature/most popular designs that we are best at. We are constantly testing out new designs and flavors and adding to the collection so check back for more. The designs are semi customizable with the size, colors, flavors, and/ or toppers added.'
+    >
+      <div className={styles.cakesContainer}>
+        <div className={styles.cakesCardRow}>
+          <ImageCard
+            title='Naked'
+            subtitle='$50 - $65'
+            image={CakeImagePlaceholder}
+            route={Routes.HOME}
+            backgroundColor='white'
+          />
+          <ImageCard
+            title='Semi-Naked'
+            subtitle='$50 - $65'
+            image={CakeImagePlaceholder}
+            route={Routes.HOME}
+            backgroundColor='white'
+          />
+          <ImageCard
+            title='Monochrome'
+            subtitle='$65 - $85'
+            image={CakeImagePlaceholder}
+            route={Routes.HOME}
+            backgroundColor='white'
+          />
+        </div>
+        <div className={styles.cakesCardRow}>
+          <ImageCard
+            title='Drip'
+            subtitle='$65 - $85'
+            image={CakeImagePlaceholder}
+            route={Routes.HOME}
+            backgroundColor='white'
+          />
+          <ImageCard
+            title='Painted Buttercream'
+            subtitle='$65 - $85'
+            image={CakeImagePlaceholder}
+            route={Routes.HOME}
+            backgroundColor='white'
+          />
+          <EmptySpace />
+        </div>
+      </div>
+    </Page>
+  );
 }
 
 export default Cakes;
