d4dccb88e1e1ae285beedb2753d41c7d09fe173a
Mohammad Anees <mohammad.irock@gmail.com>
2021-01-03 01:09:11 -0600

    add mobile nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,16 +14,25 @@ const NavButton: React.FC<{ route: Routes, label: string }> = ({ route, label })
   return (<a className={className} href={route}>{label}</a>)
 }
 
+const navButtons = () => (
+  <>
+    <NavButton route={Routes.CAKES} label='Cakes' />
+    <NavButton route={Routes.CUPCAKES} label='Cupcakes' />
+    <NavButton route={Routes.COOKIES} label='Cookies' />
+    <NavButton route={Routes.CUSTOMS} label='Customs' />
+    <NavButton route={Routes.FAQ} label='FAQ' />
+  </>
+)
+
 const Header: React.FC<HeaderProps> = () => {
+
   return (
     <div className={styles.headerContainer}>
       <a className={styles.logoButton} href={Routes.HOME}>Mo' Cakes</a>
-      <div className={styles.navContainer}>
-        <NavButton route={Routes.CAKES} label='Cakes' />-
-        <NavButton route={Routes.CUPCAKES} label='Cupcakes' />-
-        <NavButton route={Routes.COOKIES} label='Cookies' />-
-        <NavButton route={Routes.CUSTOMS} label='Customs' />-
-        <NavButton route={Routes.FAQ} label='FAQ' />
+      <div className={styles.navContainer}>{navButtons()}</div>
+      <div className={styles.mobileNavContainer}>
+        <div className={styles.navIcon}><a>Menu</a></div>
+        <div className={styles.navOptions}>{navButtons()}</div>
       </div>
     </div>
   )
