e4dd4cb29083d71013885b1d3452c5eaa6477bf1
Mohammad Anees <aman6129@neo.tamu.edu>
2021-01-02 22:23:20 -0600

    create component (#5)

---
+++ src/components/ListSection/ListSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styles from './ListSection.module.scss';
+
+interface ListItem {
+  label: string;
+  description: string;
+}
+
+interface ListSectionProps {
+  title: string;
+  listItems: ListItem[];
+  id?: string;
+}
+
+const ListSection: React.FC<ListSectionProps> = ({ title, listItems, id }) => {
+  return (
+    <div className={styles.listSection} id={id ?? 'list-section'}>
+      <h2>{title}</h2>
+      {listItems.map(({ label, description }) => (
+        <p key={`${label}-${description}`}>
+          <b>{label}</b><br/>
+          {description}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default ListSection;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as ImageCard } from './ImageCard';
 export { default as Page } from './Page';
 export { default as Footer } from './Footer';
 export { default as EmptySpace } from './EmptySpace';
+export { default as ListSection } from './ListSection';
