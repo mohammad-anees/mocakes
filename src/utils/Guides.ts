4dd51ca9e51743d62ab650c92808339240da7129
Mohammad Anees <aman6129@neo.tamu.edu>
2021-01-03 17:22:03 -0600

    add faq page (#8)

--- src/utils/Guides.ts
+++ src/utils/Guides.ts
@@ -77,4 +77,27 @@ export const CUPCAKE_FLAVORS = [
     label: 'Birthday:',
     description: 'Vanilla cake with sprinkles and white chocolate chips and a sprinkle filled vanilla buttercream',
   },
-]
+];
+
+export const FAQS = [
+  {
+    label: 'When/How to Order:',
+    description: 'It is best to place your order 2-3 weeks ahead of time for signature design/cookies. For customized orders, it is best to place your order at least 3-4 weeks ahead. Dates do book up so best to place your order as soon as possible. Fill out the inquiry form with your information and what type of dessert you need and we will try to respond to you within 24 hours.',
+  },
+  {
+    label: 'Pickup/Delivery:',
+    description: 'Pickup will be from Midtown, Houston. Instructions will be sent after order confirmation. Delivery is dependent on availability and will be an extra charge.'
+  },
+  {
+    label: 'Custom Order:',
+    description: "Custom order cakes but will focus on clean lines and minimalist style. We cannot copy and paste another artist's work but we will do our best to take that theme and build on it. Our design specializes in a modern, minimalist design with clean lines and balance."
+  },
+  {
+    label: 'Deposit/Payment:',
+    description: 'Full payment is required at order confirmation. Payment will be through Zelle or Venmo.'
+  },
+  {
+    label: 'Cancellation:',
+    description: "If you cancel within 14 days of your cake's due date, we will charge you the full price of the cake. If you cancel between 28 days and 14 days before the due date of your cake, we will charge you 50% of the price of the cake. If you cancel with more than 28 days of notice before your cake's due date, then we will offer you a full refund.",
+  }
+];
